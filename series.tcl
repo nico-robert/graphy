538cace2811cb9d73aee0f42d95aba2930e58081
nico-robert <nicolas.robert.19000@gmail.com>
2024-10-11 20:17:03 +0200

    Add Stacked bars + cosmetic changes.

--- series.tcl
+++ series.tcl
@@ -13,7 +13,6 @@ oo::class create graphy::series::Bar {
         if {[llength $args] % 2} graphy::errorEvenArgs
     
         graphy::setdef options -name            -validvalue {}  -type str       -default [graphy::seriesDefaultName barSeries [self]]
-        graphy::setdef options -type            -validvalue {}  -type str       -default "bar"
         graphy::setdef options -data            -validvalue {}  -type str       -default {}
         graphy::setdef options -startIndex      -validvalue {}  -type num       -default 0
         graphy::setdef options -indexColor      -validvalue {}  -type num|null  -default "nothing"
@@ -29,6 +28,7 @@ oo::class create graphy::series::Bar {
         graphy::setdef options -backgroundStyle -validvalue {}  -type dict      -default [graphy::backgroundStyle $args]
         graphy::setdef options -barWidth        -validvalue {}  -type num|null  -default "nothing"
         graphy::setdef options -show            -validvalue {}  -type bool      -default "True"
+        graphy::setdef options -stacked         -validvalue {}  -type bool      -default "False"
 
         set args [dict remove $args -areaStyle -label -markLine -markPoint -lineStyle -itemStyle -backgroundStyle]
         
@@ -48,6 +48,10 @@ oo::class create graphy::series::Bar {
 
     method get {} {
         return $_series
+    }
+
+    method type {} {
+        return "bar"
     }
 
 }
@@ -96,6 +100,10 @@ oo::class create graphy::series::Line {
 
     method get {} {
         return $_series
+    }
+
+    method type {} {
+        return "line"
     }
 
 }
@@ -350,27 +358,16 @@ proc graphy::lineSeries {c chart series index indexSeries y_axis_values_list max
     return $series_labels_list
 }
 
-proc graphy::barSeries {c chart series index indexSeries series_bar_len y_axis_values_list max_height xOpts} {
+proc graphy::barSeries {c chart series index indexSeries y_axis_values_list max_height xOpts} {
     
-    set optsSeries       [$series get]
     set bar_chart_margin 5.0
     set bar_chart_gap    3.0
     set decX             0.0
+    set series_bar_len   0
+    set optsSeries             [$series get]
     set series_data_count      [llength [graphy::dictGet $xOpts -data]]
     set unit_width             [expr {[$c width] / double($series_data_count)}]
     set bar_chart_margin_width [expr {$bar_chart_margin * 2.0}]
-    set bar_chart_gap_width    [expr {$bar_chart_gap * ($series_bar_len - 1)}]
-    set bar_width              [expr {($unit_width - $bar_chart_margin_width - $bar_chart_gap_width) / double($series_bar_len)}]
-    
-    if {[graphy::dictGet $optsSeries -barWidth] ne "nothing"} {
-        set bw [graphy::dictGet $optsSeries -barWidth]
-        if {$bw < $bar_width} {
-            set decX [expr {(($bar_width - $bw) / 2.0) * ($series_bar_len)}]
-            set bar_width $bw
-        }
-    }
-
-    set half_bar_width [expr {$bar_width / 2.0}]
 
     if {$index >= [llength $y_axis_values_list]} {
         set y_axis_values [lindex $y_axis_values_list 0]
@@ -393,12 +390,12 @@ proc graphy::barSeries {c chart series index indexSeries series_bar_len y_axis_v
         set radius {*}$radius
     }
 
-    set gopts         [graphy::getDictValueOrDefault $chart "color"]
-    set series_colors [graphy::dictGet $gopts color]
-    set color         [graphy::getColor $series_colors $indexcolor]
+    set gopts              [graphy::getDictValueOrDefault $chart "color"]
+    set series_colors      [graphy::dictGet $gopts color]
+    set color              [graphy::getColor $series_colors $indexcolor]
     set series_labels_list [dict create]
-    set start_index   [graphy::dictGet $optsSeries -startIndex]
-    set label_show    [graphy::dictGet $optsSeries -label show]
+    set start_index        [graphy::dictGet $optsSeries -startIndex]
+    set label_show         [graphy::dictGet $optsSeries -label show]
     set fillcolor $color
     set series_labels {}
     set i 0
@@ -433,7 +430,7 @@ proc graphy::barSeries {c chart series index indexSeries series_bar_len y_axis_v
     set showBackground [graphy::dictGet $optsSeries -showBackground]
     set tbradius       [graphy::dictTypeOf $optsSeries -backgroundStyle borderRadius]
     set rbadius        [graphy::dictGet    $optsSeries -backgroundStyle borderRadius]
-    set widthB         [graphy::dictGet    $optsSeries -backgroundStyle width]
+    set stroke_widthB  [graphy::dictGet    $optsSeries -backgroundStyle width]
     set colorB         [graphy::dictGet    $optsSeries -backgroundStyle borderColor]
     set fillcolorB     [graphy::dictGet    $optsSeries -backgroundStyle color]
     set sblurB         [graphy::dictGet    $optsSeries -backgroundStyle shadowBlur]
@@ -447,19 +444,73 @@ proc graphy::barSeries {c chart series index indexSeries series_bar_len y_axis_v
         set rbadius {*}$rbadius
     }
 
+    # Stacked bars.
+    set isStacked      [graphy::dictGet $optsSeries -stacked]
+    set series_list    [lmap mys [$chart get seriesList] {if {[$mys type] eq "bar"} {list $mys} else {continue}}]
+    set firstSeriesBar [lindex $series_list 0]
+    set index_series   [lsearch -exact $series_list $series]
+    set series_ms1     [lindex $series_list $index_series-1]
+
+    for {set j $index_series} {$j > 0} {incr j -1} {
+        set mys [lindex $series_list $j]
+        if {[graphy::dictGet [$mys get] -stacked]} {
+            incr index -1
+        }
+    }
+
+    foreach myseries $series_list {
+        # The first series does not need to be stacked.
+        if {$myseries eq $firstSeriesBar || ![graphy::dictGet [$myseries get] -stacked]} {
+            incr series_bar_len
+        }
+    }
+
+    if {!$series_bar_len} {
+        set series_bar_len 1
+    }
+
+    set bar_chart_gap_width [expr {$bar_chart_gap * ($series_bar_len - 1)}]
+    set bar_width  [expr {($unit_width - $bar_chart_margin_width - $bar_chart_gap_width) / double($series_bar_len)}]
+        
+    if {[graphy::dictGet $optsSeries -barWidth] ne "nothing"} {
+        set bw [graphy::dictGet $optsSeries -barWidth]
+        if {$bw < $bar_width} {
+            set decX [expr {(($bar_width - $bw) / 2.0) * $series_bar_len}]
+            set bar_width $bw
+        }
+    }
+
+    set half_bar_width [expr {$bar_width / 2.0}]
+
     foreach value [graphy::dictGet $optsSeries -data] {
-        if {$value in {null _}} {incr i ; continue}
+        if {$value in {null _}} {
+            if {$series_ms1 ne "" && [dict exists $infoms1 yPosBar($i)]} {
+                $series setInfo yPosBar($i) [dict get $infoms1 yPosBar($i)]
+            }
+            incr i ; continue
+        }
 
         set left [expr {$unit_width * ($i + $start_index) + $bar_chart_margin}]
         set left [expr {$left + ($bar_width + $bar_chart_gap) * $index}]
         set left [expr {$left + $decX}]
-        set y    [graphy::getOffsetHeight $y_axis_values $value $max_height]
+
+        set y      [graphy::getOffsetHeight $y_axis_values $value $max_height]
+        set height [expr {$max_height - $y}]
+
+        if {$isStacked && $series_ms1 ne ""} {
+            set infoms1 [$series_ms1 getInfo]
+            if {[dict exists $infoms1 yPosBar($i)]} {
+                set y [expr {[dict get $infoms1 yPosBar($i)] - $height}]
+            }
+        }
+
+        $series setInfo yPosBar($i) $y
 
         if {$showBackground} {
             $c rect [graphy::Component::Rect new \
                 color $colorB \
                 fill $fillcolorB \
-                stroke_width $widthB \
+                stroke_width $stroke_widthB \
                 left $left \
                 top 0 \
                 width $bar_width \
@@ -480,7 +531,7 @@ proc graphy::barSeries {c chart series index indexSeries series_bar_len y_axis_v
             top $y \
             stroke_width $stroke_width \
             width $bar_width \
-            height [expr {$max_height - $y}] \
+            height $height \
             radius $radius \
             series $series \
             sblur  $sblur \
@@ -490,9 +541,24 @@ proc graphy::barSeries {c chart series index indexSeries series_bar_len y_axis_v
         ]
 
         if {$label_show} {
+
+            set font_family [graphy::dictGet $optsSeries -label fontFamily]
+            set font_size   [graphy::dictGet $optsSeries -label fontSize]
+            set name_rotate [graphy::dictGet $optsSeries -label nameRotate]
+            set name_format [graphy::dictGet $optsSeries -label formatter]
+
+            set textformat [graphy::formatSeriesValue $value $name_format]
+            set textformat [graphy::formatString $textformat $name_format]
+
+            set b [graphy::measuretextwidth $font_family $font_size $textformat]
+            switch -exact [graphy::dictGet $optsSeries -label align] {
+                top     {set posy $y}
+                center  {set posy [expr {$y + ($height / 2.0) + [graphy::heightBox $b] / 2.0}]}
+                default {error "Unknown -label 'align' option"}
+            }
             lappend series_labels [list \
-                point [list [expr {$left + $half_bar_width}] $y] \
-                text $value \
+                point [list [expr {$left + $half_bar_width}] $posy] \
+                text $textformat \
             ]
         }
         incr i
